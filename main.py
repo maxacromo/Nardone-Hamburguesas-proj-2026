e36b1c16e42c9021697f6ed7c07d0accf93bc1b2
hernancastro1234 <castrohernan44@gmail.com>
2026-04-05 19:58:42 -0300

    probando

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-from empleados.funciones import mostrar_menu_principal
 
-mostrar_menu_principal()
+from empleados.funciones import menu_principal
+from empleados.usuarios import empleados, atributo_empleados
+
+def main():
+    print("Bienvenidos al sistema de administración")
+    menu_principal(empleados, atributo_empleados)
+
+if __name__ == "__main__":
+    main()
