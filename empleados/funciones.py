196f05b32176c83cd67da82d84ec31c990356422
hernancastro1234 <114106799+hernancastro1234@users.noreply.github.com>
2026-04-06 18:43:35 -0300

    menu
    colores de menu

--- empleados/funciones.py
+++ empleados/funciones.py
@@ -38,8 +38,8 @@ def validar_usuario(empleados,usuario,contra, atributo_empleados):
 def login(empleados, atributo_empleados):
     limpiar_pantalla()
     dibujar_borde(" INICIAR SESIÓN ", 40)
-    usuario = input("  Usuario: ").strip()
-    contra = input("  Contraseña: ").strip()  
+    usuario = input("  \033[1;37mUsuario: \033[0m").strip()
+    contra = input("  \033[1;37mContraseña: \033[0m").strip()  
     validar_usuario(empleados,usuario,contra, atributo_empleados)
 
 #---------------------------------------------------------------
@@ -62,14 +62,14 @@ def registro():
 def menu_principal(empleados, atributo_empleados):
     while True:
         limpiar_pantalla()
-        dibujar_borde(" HAMBURGESAS ", 40)
-        print("  1. Iniciar sesión")
-        print("  2. Registrarse")
-        print("  3. Ver créditos")
-        print("  4. Salir")
+        dibujar_borde(" \033[1;33m🍔 HAMBURGESAS 🍔\033[0m", 40)
+        print("  \033[1;34m1. Iniciar sesión\033[0m")
+        print("  \033[1;34m2. Registrarse\033[0m")
+        print("  \033[1;34m3. Ver créditos\033[0m")
+        print("  \033[1;34m4. Salir\033[0m")
         print()
         
-        opcion = input("  → Elige una opción (1-4): ").strip()
+        opcion = input("\033[1;37m  → Elige una opción (1-4): \033[0m")
 
         #opcion de menu
         if opcion == "1":
@@ -85,41 +85,43 @@ def menu_principal(empleados, atributo_empleados):
             print("\n  Nos vemos nene, cuidate")
             break
         else:
-            print("  Opción inválida... intenta de nuevo")
+            print("  \033[1;31mOpción inválida... intenta de nuevo\033[0m")
 ## --------------------------------------------------------------
 
 
 #---------------------------------------------------------------
 #MENU QUE VISUALIZA EL ADMINISTRADOR 
 #---------------------------------------------------------------
+
+    
 def submenu_admin(empleados, atributo_empleados):
     while True:
         limpiar_pantalla()
-        print("1.Listar Usuarios ")
-        print("2.Crear usuarios")
-        print("3.Modificar usuario")
+        print("  \033[1;34m1. Listar Usuarios\033[0m")
+        print("  \033[1;34m2. Crear usuarios\033[0m")
+        print("  \033[1;34m3. Modificar usuario\033[0m")
         ##############################################
         #EZE acá esta ventas
-        print("4.Módulo de ventas")
-        print("5.Ver estadisticas")
-        print("6.Salir")
-        opcion=input("Ingrese el numero de opcion : ")
+        print("  \033[1;34m4. Módulo de ventas\033[0m")
+        print("  \033[1;34m5. Ver estadisticas\033[0m")
+        print("  \033[1;34m6. Salir\033[0m")
+        opcion=input("\033[1;37mIngrese el numero de opcion : \033[0m")
         limpiar_pantalla()
         if opcion=="1":
-            print("Listar Usuarios")
+            print(" \033[1;34mListar Usuarios\033[0")
             mostrar_empleados(empleados, atributo_empleados)
-            input("Presione enter para volver al menu ")
+            input("\n\033[1;34mPresione Enter para volver al menu...\033[0m")
             limpiar_pantalla()
         elif opcion=="2":
             limpiar_pantalla()
             print("Crear usuarios")
             agregar_empleado(empleados)
-            input("Presione enter para volver al menu")
+            input("\033[1;34mPresione Enter para volver al menu...\033[0m")
             limpiar_pantalla()
         elif opcion=="3":
             print("Modificar usuario")
             modificar_usuario(empleados,atributo_empleados)
-            input("Presione enter para volver al menu.")
+            input("\033[1;34mPresione enter para volver al menu.\033[0m")
             limpiar_pantalla()
         elif opcion=="4":
             print("Ver ventas")
@@ -162,9 +164,9 @@ def validacion_letras(mensaje, campo):
     while True:
         valor = input(mensaje).strip()
         if valor == "":
-            print(f"El {campo} no puede estar vacío.")
+            print("\033[1;34m"+f"El {campo} no puede estar vacío.\033[0m")
         elif not valor.isalpha():
-            print(f"El {campo} no puede contener números o caracteres especiales.")
+            print("\033[1;31m"+f"El {campo} no puede contener números o caracteres especiales.\033[0m")
         else:
             return valor
 
@@ -175,15 +177,15 @@ def validacion_usuario(mensaje):
     while True:
         usuario=input(mensaje).strip()
         if usuario=="":
-            print("El nombre de usuario no puede estar vacio.")
+            print("\033[1;31mEl nombre de usuario no puede estar vacio.\033[0m")
         elif len(usuario)<5 or len(usuario)>8:
-            print("El nombre de usuario debe tener entre 5 y 8 caracteres.")
+            print("\033[1;33mEl nombre de usuario debe tener entre 5 y 8 caracteres.\033[0m")
         elif not usuario.isalnum():
-            print("El nombre de usuario no puede contener caracteres especiales.")
+            print("\033[1;31mEl nombre de usuario no puede contener caracteres especiales.\033[0m")
         elif usuario.isnumeric():
-            print("El nombre de usuario no puede contener solo números.")
+            print("\033[1;31mEl nombre de usuario no puede contener solo números.\033[0m")
         elif usuario.isalpha():
-            print("El nombre de usuario no puede contener solo letras.")    
+            print("\033[1;31mEl nombre de usuario no puede contener solo letras.\033[0m")
         else:
             return usuario
         
@@ -194,13 +196,13 @@ def validacion_password(mensaje):
     while True:
         password=input(mensaje).strip()
         if password =="":
-            print("La contraseña no puede estar vacia")
+            print("\033[1;31mLa contraseña no puede estar vacia\033[0m")
         elif len(password)<5 or len(password) >10:
-            print("La contraseña debe contener entre 5 y 10 caractes")
+            print("\033[1;33mLa contraseña debe contener entre 5 y 10 caracteres\033[0m")
         elif password.isalpha():
-            print("La contraseña no puede tener solo letras")
+            print("\033[1;33mLa contraseña no puede tener solo letras\033[0m")
         elif password.isnumeric():
-            print("La contraseña no puede contener solo numeros")
+            print("\033[1;33mLa contraseña no puede contener solo numeros\033[0m")
         else:
             return password
         
@@ -212,9 +214,9 @@ def validacion_rol(mensaje):
     while True:
         rol=input(mensaje).strip()
         if rol=="":
-            print("El rol no puede encontrarse vacio")
+            print("\033[1;31mEl rol no puede encontrarse vacio\033[0m")
         elif rol not in ["1","2"]:#valida que el rol ingresado sea 1 o 2
-            print("Rol no válido. Ingrese 1 para admin o 2 para empleado.")
+            print("\033[1;31mRol no válido. Ingrese 1 para admin o 2 para empleado.\033[0m")
         else:
             return "admin" if rol=="1" else "empleado"
 
@@ -237,15 +239,15 @@ def obtener_id(empleados):
 #---------------------------------------------------------------
 def agregar_empleado(empleados):
     while True:
-        nombre=validacion_letras("Ingrese el nombre del empleado: ","nombre")
-        apellido=validacion_letras("Ingrese el apellido del empleado: ","apellido")
-        user=validacion_usuario("Ingrese el nombre de usuario: ")
-        password=validacion_password("Ingrese la contraseña: ")     
-        rol=validacion_rol("Ingrese el rol del usuario 1-admin o 2-empleado: ")
+        nombre=validacion_letras("\033[1;37mIngrese el nombre del empleado: \033[0m", "nombre")
+        apellido=validacion_letras("\033[1;37mIngrese el apellido del empleado: \033[0m", "apellido")
+        user=validacion_usuario("\033[1;37mIngrese el nombre de usuario: \033[0m")
+        password=validacion_password("\033[1;37mIngrese la contraseña: \033[0m")     
+        rol=validacion_rol("\033[1;37mIngrese el rol del usuario 1-admin o 2-empleado: \033[0m")
         nuevo_id=obtener_id(empleados)
         estado="Activo"  
         empleados.append([nuevo_id,nombre,apellido,user,rol,password,estado])#agrega una nueva fila a la matriz de empleados con los datos ingresados
-        salida=str(input("Para finalizar la carga de usuarios presione X o enter para seguir: ")).lower()
+        salida=str(input("\033[1;34mPara finalizar la carga de usuarios presione X o enter para seguir: \033[0m")).lower()
         if salida=="x":
             return 
      
@@ -254,55 +256,58 @@ def agregar_empleado(empleados):
 #---------------------------------------------------------------
 def mostrar_empleados(empleados,atributo_empleados):
     for atributo in atributo_empleados:
-        print(f"{atributo:<15}", end=" ")#esto hace que se impriman uno a lado del otro, en controla el final del print.
+        print("\033[1;33m"f"{atributo:<15}\033[0m", end=" ")#esto hace que se impriman uno a lado del otro, en controla el final del print.
+         
     print("\n")
+    #print("\033[32m" + "─" * 112 + "\033[0m") 
     for empleado  in empleados:#recorre cada fila (empleado)
+        print("\033[1;35m" + "─" * 105 + "\033[0m")
         for dato in empleado:#recorre cada valor dentro de esa fila.
-            print(f"{dato:<15}", end=" ")#esto hace que se impriman uno a lado del otro, en controla el final del print.")
+            print("\033[1;37m"f"{dato:<15}\033[0m", end=" ")#esto hace que se impriman uno a lado del otro, en controla el final del print.")
         print()
+        print("\033[1;35m" + "─" * 105 + "\033[0m")
 
 #---------------------------------------------------------------
 # Función para modificar el nombre de usuario de un empleado
 #---------------------------------------------------------------
-
 def modificar_usuario(empleados,atributo_empleados):
     mostrar_empleados(empleados,atributo_empleados)
-    id_buscado = int(input("Ingrese el ID del empleado a modificar: "))
+    id_buscado = int(input("\033[1;34mIngrese el ID del empleado a modificar: \033[0m"))
     for empleado in empleados:
         if empleado[ID] == id_buscado:
             print(empleado)
-            print("Ingrese el numero del atributo que desea modificar: ")
-            print("1.Nombre")
-            print("2.Apellido")
-            print("3.Usuario")
-            print("4.Rol")
-            print("5.Contraseña")
-            print("6.Estado del usuario")
-            opcion=input("Ingrese el numero de la opción: ")
+            print(" \033[1;33mIngrese el numero del atributo que desea modificar: \033")
+            print(" \033[1;34m1.Nombre\033[0m")
+            print(" \033[1;34m2.Apellido\033[0m")
+            print(" \033[1;34m3.Usuario\033[0m")
+            print(" \033[1;34m4.Rol\033[0m")
+            print(" \033[1;34m5.Contraseña\033[0m")
+            print(" \033[1;34m6.Estado del usuario\033[0m")
+            opcion=input("\033[1;33mIngrese el numero de la opción: \033[0m")
             if opcion=="1":
-                nuevo_nombre=validacion_letras("Ingrese el nuevo nombre ", "nombre")
+                nuevo_nombre=validacion_letras("\033[1;34mIngrese el nuevo nombre: \033[0m", "nombre")
                 empleado[NOMBRE]=nuevo_nombre
 
             elif opcion=="2":
-                nuevo_apellido=validacion_letras("Ingrese el nuevo apellido ", "apellido")
+                nuevo_apellido=validacion_letras("\033[1;34mIngrese el nuevo apellido: \033[0m", "apellido")
                 empleado[APELLIDO]=nuevo_apellido
             elif opcion=="3":
-                nuevo_usuario=validacion_usuario("Ingrese el nuevo usuario: ")
+                nuevo_usuario=validacion_usuario("\033[1;34mIngrese el nuevo usuario: \033[0m")
                 empleado[USUARIO]=nuevo_usuario
             elif opcion=="4":
-                nuevo_rol=validacion_rol("Ingrese el nuevo  rol asignado ,  1 para admin o 2 para empleado: ")
+                nuevo_rol=validacion_rol("\033[1;34mIngrese el nuevo rol asignado ,  1 para admin o 2 para empleado: \033[0m")
                 empleado[ROL]=nuevo_rol
             elif opcion=="5":
-                nueva_pass=validacion_password("Ingrese la nueva contraseña: ")
+                nueva_pass=validacion_password("\033[1;34mIngrese la nueva contraseña: \033[0m")
                 empleado[PASSWORD]=nueva_pass
             elif opcion=="6":
                 estado_actualizado=modificar_estado(empleados)
                 empleado[ESTADO]=estado_actualizado
             else:
-                print("Opcion invalida")
-            return print("Modificado con exito \n",empleado)
+                print("\033[1;31mOpcion invalida\033[0m")
+            return print("\033[1;32mModificado con exito \n\033[0m",empleado)
 
-    print("Empleado no encontrado")
+    print("\033[1;31mEmpleado no encontrado\033[0m")
 #-----------------------------------------------------------
 """
 Se toma la decisión de no eliminar ningun usuario dado que generaria una inconsistencia en el sistema.
@@ -313,8 +318,8 @@ se encuentra "activo" podra iniciar sesión
     
 def modificar_estado(empleados):
     while True:
-        nuevo_estado=input("Ingrese el nuevo estado. 1 activo y 2 para Inactivo: ").strip()
+        nuevo_estado=input("\033[1;37mIngrese el nuevo estado. 1 activo y 2 para Inactivo: \033[0m").strip()
         if nuevo_estado not in ["1","2"]:
-            print("Opcion no valida, ingrese 1 para activo o 2 para inactivo")
+            print("\033[1;31mOpcion no valida, ingrese 1 para activo o 2 para inactivo\033[0m")
         else:
-            return "Activo" if nuevo_estado=="1" else "Inactivo"
+            return "\033[1;32mActivo\033[0m" if nuevo_estado=="1" else "\033[1;31mInactivo\033[0m"
