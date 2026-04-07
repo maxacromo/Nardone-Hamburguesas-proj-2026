8c5a74bf8e7ab32f8bb644ccbb260285383fb58b
M.Masi <maximoalejandromasi@gmail.com>
2026-04-07 16:53:06 -0300

    Pantallas limpias, Bajas Logicas y Correos

--- Clientes.py
+++ Clientes.py
@@ -3,62 +3,60 @@ import random, pprint
 
 def Persona_Relleno(): #Genera Personas no reales para que el sistema cargue con Cliente ya existen
     Persona_ID=[]
+    Mail_List=[]
+    Cliente={}
+    cycle=random.randint(5,10)
+    cont=0
+    while cont != cycle:
+        Persona=Creador_Personas(Mail_List) 
 
-    P_Random=random.randint(5,10)
-    Persona=Creador_Personas(P_Random) 
+        def Creador_Usuarios():
+            Num_list(Persona_ID)
+            Cliente[Persona_ID[cont]]=Persona
+            return Cliente
+        Creador_Usuarios()
+        cont+=1
 
-    def Creador_Usuarios():
-        Num_list(Persona,Persona_ID)
-        Cliente={}
-        for i in range(len(Persona)):
-            Cliente[Persona_ID[i]]=Persona[i]
-        return Cliente
-    
-
-    Cliente= Creador_Usuarios()
-    return Cliente, Persona_ID
+    return Cliente, Persona_ID, Mail_List
 
 
-def Crear_Cliente(Persona_ID,Cliente): #Funcion que permite agregar Cliente
-    Aux={}
+def Crear_Cliente(Persona_ID,Cliente,Mail_List): #Funcion que permite agregar Cliente
     Hold=""
     Flag=0
     while Flag !=1:
+        Aux={}
+        Persona=[]
         Flag=0
         Confirm=input("Quiere agregar un nuevo cliente? y/n:").strip()
         if Confirm == "yes" or Confirm == "y":
             Hold=Gen_FullName(Hold)
+            Pila=Hold.split(" ",1)[0]
+            Apellido=Hold.split(" ",1)[1]
+
+            Mail=Gen_Mail(Mail_List,Pila,Apellido)
+            Persona.extend([Hold,Mail,"A"])
+
+            Num_list(Persona_ID)
+            contador=len(Persona_ID)
+            Aux[Persona_ID[contador-1]]=Persona
+            Cliente.update(Aux)
 
         elif Confirm == "no" or Confirm == "n":
+            input("Presione enter para volver al menu previo.")
+            limpiar_pantalla()
             Flag=1
 
         else:
+            limpiar_pantalla()
             print("Error, realice un ingreso valido")
             Flag=2
             
 
-
-        if Flag==1 or Flag==2:
-            pass
-        else:
-            nro=-1
-            flag=0
-            while flag==0:
-                if nro in Persona_ID or nro==-1:
-                    nro=random.randint(100000,999999)
-                else:
-                    Persona_ID.append(nro)
-                    flag=1
-            contador=len(Persona_ID)
-            Aux[Persona_ID[contador-1]]=Hold
-            Cliente.update(Aux)
-
-
- 
-
 def Read_Cliente(Cliente): #Permite leer la matriz de clientes
                             #Para interfaz si quieren sacar el pretty print y poner otra cosa, no se va a romper
     pprint.pprint(Cliente)
+    input("Presione enter para volver al menu previo.")
+    limpiar_pantalla()
        
 
 def Update_Cliente(Cliente): #Permite cambiar nombres de usuario pero si a alguien se le ocurre algun valor mas se le puede agregar
@@ -75,79 +73,127 @@ def Update_Cliente(Cliente): #Permite cambiar nombres de usuario pero si a algui
             Check,Search,Result=Verificacion_Mod_Usuario(Cliente)
             if Check=="y":
                 print("A que quiere cambiar el nombre?")
-                Change=""
-                Change=Gen_Nombre(Change,Result)
-                Cliente[Search]=Change
+                Gen_Nombre(Result)
+                Cliente[Search]=Result
             
             elif Check =="n":
                 Check="1"
+                input("Presione enter para volver al menu previo.")
+                limpiar_pantalla()
                 pass
             else:
+                limpiar_pantalla()
                 print("Error,ingrese un input valido")
         
         elif Check=="2":
             Check,Search,Result=Verificacion_Mod_Usuario(Cliente)
             if Check=="y":
                 print("A que quiere cambiar el nombre?")
-                Change=""
-                Change=Gen_Apellido(Change,Result)
-                Cliente[Search]=Change
+                Gen_Apellido(Result)
+                Cliente[Search]=Result
             
             elif Check =="n":
                 Check="1"
+                input("Presione enter para volver al menu previo.")
+                limpiar_pantalla()
                 pass
             else:
+                limpiar_pantalla()
                 print("Error,ingrese un input valido")
             
         elif Check=="3":
             Check,Search,Result=Verificacion_Mod_Usuario(Cliente)
             if Check=="y":
                 print("A que quiere cambiar el nombre?")
-                Change=""
-                Change=Gen_FullName(Change)
-                Cliente[Search]=Change
+                Gen_FullName(Result)
+                Cliente[Search]=Result
             
             elif Check =="n":
                 Check="1"
+                input("Presione enter para volver al menu previo.")
+                limpiar_pantalla()
                 pass
             else:
+                limpiar_pantalla()
                 print("Error,ingrese un input valido")
 
         elif Check=="4":
             Flag=1
+            input("Presione enter para volver al menu previo.")
+            limpiar_pantalla()
 
         else:
+            limpiar_pantalla()
             print("Error,ingrese un input valido")
 
     return Cliente
 
 def Destruir_Cliente(Cliente):# Permite Borrar clientes
-    Flags=[]
-    while 0 not in Flags:
-        Flags.clear()
-        Check=input("Quiere Borrar un cliente? y/n:")
-        if Check=="y":
-            Check,Search=Verificacion_Mod_Usuario(Cliente)
+    Flag=0
+    while Flag ==0:
+        print("1)Borrar Cliente")
+        print("2)Reactivar Cuenta")
+        print("3)Salir")
+        Check=input(":")
+        if Check=="1":
+            Check,Search,Result=Verificacion_Mod_Usuario(Cliente)
             if Check == "y":
-                Cliente.pop(Search)
+                Cliente[Search][2]="IN"
+                input("Presione enter para volver al menu previo.")
+                limpiar_pantalla()
             
             elif Check =="n":
                 Check="1"
                 pass
             else:
+                limpiar_pantalla()
                 print("Error,ingrese un input valido")
+        elif Check=="2":
+            print("Ingrese el ID del cliente que quiere modificar, o Ingrese -1 para salir", end=" ")
+            Search=input(":")
+            if Search =="-1":
+                limpiar_pantalla()
+                return "n",0,0
+            elif not Search.isnumeric():
+                print("Error, ingrese un numero")
+                return "n",0,0
+            else:
+                Search=int(Search)
+                Result=Search_User(Search,Cliente)
 
-        elif Check=="n":
-            Flags.append(0)
+                if Result=="Not Found":
+                    print("Ese ID no esta en la base de datos")
+                    return "n",0,0
+                else:
+                    print("Quiere modificar al cliente",Search,Result,"y/n",end=" ")
+                    Check= input(":").strip()
+                    
+            if Check == "y":
+                Cliente[Search][2]="A"
+                input("Presione enter para volver al menu previo.")
+                limpiar_pantalla()
+            
+            elif Check =="n":
+                Check="1"
+                pass
+            else:
+                limpiar_pantalla()
+                print("Error,ingrese un input valido")
+        
+        elif Check=="3":
+            Flag=1
+            input("Presione enter para volver al menu previo.")
+            limpiar_pantalla()
 
         else:
+            limpiar_pantalla()
             print("Error,ingrese un input valido")
 
     return Cliente
 
 
-Cliente,Persona_ID =Persona_Relleno()
-
+Cliente,Persona_ID,Mail_List =Persona_Relleno()
+limpiar_pantalla()
 while True:
     print()
     print("Bienvenido!")
@@ -159,18 +205,24 @@ while True:
     print("5) Salir")
     Check= input(":")
     if Check=="1":
-        Crear_Cliente(Persona_ID,Cliente)
+        limpiar_pantalla()
+        Crear_Cliente(Persona_ID,Cliente,Mail_List)
     
     elif Check =="2":
+        limpiar_pantalla()
         Read_Cliente(Cliente)
     
     elif Check=="3":
+        limpiar_pantalla()
         Update_Cliente(Cliente)
     
     elif Check=="4":
-        Cliente = Destruir_Cliente(Cliente)
+        limpiar_pantalla()
+        Destruir_Cliente(Cliente)
     elif Check=="5":
         print("Bye!")
+        Cleanup(Cliente,Persona_ID)
         break
     else :
+        limpiar_pantalla()
         print("Error,ingrese un input valido") 
--- Clientes_Funciones.py
+++ Clientes_Funciones.py
@@ -1,23 +1,21 @@
-import random
+import random, os
 
-def Creador_Personas(cycle): #Esta funcion toma un nombre y un apellido de las listas para generar a una persona con un nombre aleatorio
-                                 #Además Carga a las personas a una lista q luego es pasada a otra lista en el llamado de función 
-        cont=0
-        Persona=[]
-        Random_Names=lambda: random.choice(Pila) + " " + random.choice(Apellidos)
+def Creador_Personas(Mail_List): #Esta funcion toma un nombre y un apellido de las listas para generar a una persona con un nombre aleatorio
+                                 #Además Carga a las personas a una lista q luego es pasada a otra lista en el llamado de función      
+    Persona=[]
+    L_Pila=["María","Ana","Lucia","Martina","Daniela","Julia","Luciana","Sofia", "Jose","Luis","Carlos","Juan","Martin","Julian","Esteban","Roberto"]
+    L_Apellidos=["González","Rodríguez","Gómez","Fernández","López","Díaz","Martínez","Pérez","Sánchez","Romero","García","Sosa","Benítez","Ramírez","Ruiz","Torres"]
 
-        while cont != cycle:
-            Pila=["María","Ana","Lucia","Martina","Daniela","Julia","Luciana","Sofia", "Jose","Luis","Carlos","Juan","Martin","Julian","Esteban","Roberto"]
-            Apellidos=["González","Rodríguez","Gómez","Fernández","López","Díaz","Martínez","Pérez","Sánchez","Romero","García","Sosa","Benítez","Ramírez","Ruiz","Torres"]
+    Pila=random.choice(L_Pila) 
+    Apellido=random.choice(L_Apellidos)
+    Nombre=Pila+" "+Apellido
+    Mail=Gen_Mail(Mail_List,Pila,Apellido)
+    Persona.extend([Nombre,Mail,"A"])
 
-        
-            Persona.append(Random_Names())
-            cont+=1
+    return Persona
 
-        return Persona
 
-def Num_list(Persona,Persona_ID):# Crea ID de Usuario para las personas de relleno
-        for i in range(len(Persona)):
+def Num_list(Persona_ID):# Crea ID de Usuario para las personas de relleno
             nro=-1
             flag=0
             Check_ID=lambda x: False if x in Persona_ID else True
@@ -29,7 +27,7 @@ def Num_list(Persona,Persona_ID):# Crea ID de Usuario para las personas de relle
                     Persona_ID.append(nro)
                     flag=1
 
-def Gen_FullName (Fullname):#Se utiliza para el input de nombres de parte de los usuarioss
+def Gen_FullName (Result):#Se utiliza para el input de nombres de parte de los usuarioss
     Flag=0
     while Flag==0:
         Pila=input("Inserte el nombre de pila del cliente:")
@@ -40,63 +38,62 @@ def Gen_FullName (Fullname):#Se utiliza para el input de nombres de parte de los
         print()
 
         if Confirm == "yes" or Confirm == "y":
-            Fullname=Nombre
+            Result[0]=Nombre
             Flag=1
 
         elif Confirm == "no" or Confirm == "n":
             Flag=1
 
         else:
+            limpiar_pantalla()
             print("Error, realice un ingreso valido")
-    return Fullname
+    return Result
 
-def Gen_Nombre (Aux,Result):#Se utiliza para el input de nombres de parte de los Cliente
+def Gen_Nombre (Result):#Se utiliza para el input de nombres de parte de los Cliente
     Flag=0
     while Flag==0:
         Pila=input("Inserte el nombre de pila del cliente:")
-        Nombre= Pila + " " + Result.split(" ",1)[1]
+        Apellido=Result[0].split(" ",1)[1]
+        Nombre= Pila + " " + Apellido
         print("Su nombre es ", Nombre, "? y/n", end=" ")
         Confirm=input(":").strip()
         print()
 
         if Confirm == "yes" or Confirm == "y":
-            Aux=Nombre
+            Result[0]=Nombre
             Flag=1
 
         elif Confirm == "no" or Confirm == "n":
-            Aux=Result
             Flag=1
 
         else:
             print("Error, realice un ingreso valido")
-    return Aux
+    return Result
 
-def Gen_Apellido (Aux,Result):#Se utiliza para el input de nombres de parte de los Cliente
+def Gen_Apellido (Result):#Se utiliza para el input de nombres de parte de los Cliente
     Flag=0
     while Flag==0:
+        Pila=Result[0].split(" ",1)[0]
         Apellido=input("Inserte el apellido del cliente:")
-        Nombre= Result.split(" ",1)[0] + " " + Apellido
+        Nombre= Pila + " " + Apellido
         print("Su nombre es ", Nombre, "? y/n", end=" ")
         Confirm=input(":").strip()
         print()
 
         if Confirm == "yes" or Confirm == "y":
-            Aux=Nombre
+            Result[0]=Nombre
             Flag=1
 
         elif Confirm == "no" or Confirm == "n":
-            Aux=Result
             Flag=1
 
         else:
             print("Error, realice un ingreso valido")
-    return Aux 
+    return Result
 
 
 def Search_User(Search,Cliente):#Ubica las posciciones del id de los Cliente
-    print(Cliente[Search])
     if Search in Cliente:
-        print("Found")
         Result=Cliente[Search]
         return Result
     else:
@@ -107,19 +104,52 @@ def Search_User(Search,Cliente):#Ubica las posciciones del id de los Cliente
 def Verificacion_Mod_Usuario(Cliente):
     print("Ingrese el ID del cliente que quiere modificar, o Ingrese -1 para salir", end=" ")
     Search=input(":")
-    if not Search.isdigit():
+    if Search =="-1":
+        limpiar_pantalla()
+        return "n",0,0
+    elif not Search.isnumeric():
         print("Error, ingrese un numero")
         return "n",0,0
     else:
         Search=int(Search)
-        if Search ==-1:
-            return "n",0,0
         Result=Search_User(Search,Cliente)
 
         if Result=="Not Found":
             print("Ese ID no esta en la base de datos")
+            return "n",0,0
+        
+        elif Result[2]=="IN":
+            print("Esta cuenta esta inactiva")
+            return "n",0,0
 
         else:
             print("Quiere modificar al cliente",Search,Result,"y/n",end=" ")
             Check= input(":").strip()
             return Check, Search, Result
+
+def limpiar_pantalla():
+    if os.name == "nt":  # Windows
+        #Es una funcion de python que ejecuta comandos del sistema operativo.
+        os.system("cls")#Le pasamos el comando y lo ejectua en la shell
+    else: #Liempia la pantalla Linux y Mac
+        os.system("clear")
+
+Lowercase= lambda x: x.lower()
+
+def Gen_Mail(Mail_List,Pila,Apellido):
+    Mail_Root=Lowercase(Pila)[:1]+Lowercase(Apellido)
+    Mailatt="@gmail.com"
+    Mail=Mail_Root+Mailatt
+    cont=1
+    while Mail in Mail_List:
+        cont=str(cont)
+        Mail=Mail_Root+cont+Mailatt
+        cont=int(cont)
+        cont+=1
+    Mail_List.append(Mail)
+    return Mail
+
+def Cleanup(Cliente,Persona_ID):
+    for i in range(len(Persona_ID)):
+        if "IN" in Cliente[Persona_ID[i]]:
+            Cliente.pop(Persona_ID[i])
