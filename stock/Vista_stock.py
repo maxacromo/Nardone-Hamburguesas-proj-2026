e246d13ef7911bbebb4c9451433c434682c9ffd3
Santiago <144866430+Elcano03@users.noreply.github.com>
2026-04-21 14:30:46 -0300

    especifico en el print de buscar ingrediente por nombre

--- stock/Vista_stock.py
+++ stock/Vista_stock.py
@@ -8,7 +8,7 @@ def inicio_stock():
     print("[2] Borrar ingrediente")
     print("[3] Modificar stock de un ingrediente")
     print("[4] Listar ingredientes")
-    print("[5] Buscar ingrediente en stock")
+    print("[5] Buscar ingrediente por nombre en stock")
     print("-"*ancho_menu)
     print("[0] Salir")
     print("-"*ancho_menu)
